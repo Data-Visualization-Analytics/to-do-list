31c6be263df2f4d74686a2cd0486ec768483155e
rhnmto <rohankumarmahato@gmail.com>
2019-07-09 01:29:16 +0530

    Added Procfile

--- app.js
+++ app.js
@@ -39,6 +39,6 @@ app.post("/", (req, res) => {
 
 });
 
-app.listen(process.env.PORT, (req, res) => {
+app.listen(process.env.PORT || 3000, (req, res) => {
   console.log("Server is running on port 3000");
 });
